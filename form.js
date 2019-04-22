001c6d7c3c5a25fb0833807587d0e6e2bae12085
Irwinda <mesanda@gmail.com>
2019-04-22 22:29:31 +0700

    First commit

---
+++ form.js
@@ -0,0 +1,79 @@
+function checkOverlapping(selected_jadwal){
+	var jadwal = {"Gardening": "Senin", "Reading": "Senin", "Running":"Kamis", "Archery":"Rabu", "Photography":"Jumat", "Yoga":"Kamis"};
+    var pickup = jadwal;
+
+	for(var key in jadwal) {
+    if (!(selected_jadwal.indexOf(key) > -1)){
+         delete pickup[key]
+    }
+	}
+
+   for(var key1 in pickup){
+     for(var key2 in pickup){
+       if (pickup[key1] == pickup[key2] && key1 != key2){
+         return "Mohon maaf, kegiatan " + key1 + " berada dengan hari yang sama dengan kegiatan " + key2 + ", silahkan pilih kegiatan yang lain :)"
+       }
+     }
+   }
+
+   return ""
+}
+
+
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var emailValid = re.test(String(email).toLowerCase());
+
+    if (emailValid) {
+      return     } else {
+      return "format email salah, silahkan ketikkan kembali dengan benar"
+    }
+}
+
+
+function calculateTotal(selected_jadwal){
+	var harga = {"Gardening": 25000, "Reading":35000, "Running":50000, "Archery":25000, "Photography":50000, "Yoga":250000};
+	var pickup = harga;
+	var total = 0;
+
+  for (var index = 0; index < selected_jadwal.length; ++index) {
+ 
+      total = total + harga[selected_jadwal[index]];
+  }
+  return total
+}
+
+function submitform(){
+  var name = document.getElementById("full-name").value;
+  var email = document.getElementById("email").value;
+  var nohp = document.getElementById("nohp").value;
+  var dropdown = document.getElementById("kompartemen");
+  var choices = dropdown.options[dropdown.selectedIndex].value;
+  var selectedGender = document.querySelector('input[name="gender"]:checked').value;
+  var checkedValue =[];
+  var inputElements = document.getElementsByClassName('messageCheckbox');
+  for(var i=0; inputElements[i]; ++i){
+    if(inputElements[i].checked){
+      checkedValue.push(inputElements[i].value)
+      // break;
+    }
+  }
+
+
+// console.log(calculateTotal(checkedValue));
+// console.log(checkOverlapping(checkedValue));
+
+// console.log(validateEmail(email));
+// validateEmail();
+
+  // console.log(name);
+  // console.log(dropdown);
+  // console.log(email);
+  // console.log(nohp);
+  // console.log(selectedGender);
+  // console.log(choices);
+  // console.log(checkedValue);
+ 
+
+}
+
